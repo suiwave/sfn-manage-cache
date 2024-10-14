3c4451e0ab4000eb1715a5ab234fc937cc00c757
suiwave <184898096+suiwave@users.noreply.github.com>
2024-10-15 07:40:59 +0900

    vpc lambda作成

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,44 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.6.0"
+  hashes = [
+    "h1:rYAubRk7UHC/fzYqFV/VHc+7VIY01ugCxauyTYCNf9E=",
+    "zh:29273484f7423b7c5b3f5df34ccfc53e52bb5e3d7f46a81b65908e7a8fd69072",
+    "zh:3cba58ec3aea5f301caf2acc31e184c55d994cc648126cac39c63ae509a14179",
+    "zh:55170cd17dbfdea842852c6ae2416d057fec631ba49f3bb6466a7268cd39130e",
+    "zh:7197db402ba35631930c3a4814520f0ebe980ae3acb7f8b5a6f70ec90dc4a388",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8bf7fe0915d7fb152a3a6b9162614d2ec82749a06dba13fab3f98d33c020ec4f",
+    "zh:8ce811844fd53adb0dabc9a541f8cb43aacfa7d8e39324e4bd3592b3428f5bfb",
+    "zh:bca795bca815b8ac90e3054c0a9ab1ccfb16eedbb3418f8ad473fc5ad6bf0ef7",
+    "zh:d9355a18df5a36cf19580748b23249de2eb445c231c36a353709f8f40a6c8432",
+    "zh:dc32cc32cfd8abf8752d34f2a783de0d3f7200c573b885ecb64ece5acea173b4",
+    "zh:ef498e20391bf7a280d0fd6fd6675621c85fbe4e92f0f517ae4394747db89bde",
+    "zh:f2bc5226c765b0c8055a7b6207d0fe1eb9484e3ec8880649d158827ac6ed3b22",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.33.0"
+  constraints = ">= 5.30.0, ~> 5.33.0"
+  hashes = [
+    "h1:rAmKVvvzUqVocFppyheelWGnyfCcIGxLV31iFBY2sz4=",
+    "zh:10bb683f2a9306e881f51a971ad3b2bb654ac94b54945dd63769876a343b5b04",
+    "zh:3916406db958d5487ea0c2d2320012d1907c29e6d01bf693560fe05e38ee0601",
+    "zh:3cb54b76b2f9e30620f3281ab7fb20633b1e4584fc84cc4ecd5752546252e86f",
+    "zh:513bcfd6971482215c5d64725189f875cbcbd260c6d11f0da4d66321efd93a92",
+    "zh:545a34427ebe7a950056627e7c980c9ba16318bf086d300eb808ffc41c52b7a8",
+    "zh:5a44b90faf1c8e8269f389c04bfac25ad4766d26360e7f7ac371be12a442981c",
+    "zh:64e1ef83162f78538dccad8b035577738851395ba774d6919cb21eb465a21e3a",
+    "zh:7315c70cb6b7f975471ea6129474639a08c58c071afc95a36cfaa41a13ae7fb9",
+    "zh:9806faae58938d638b757f54414400be998dddb45edfd4a29c85e827111dc93d",
+    "zh:997fa2e2db242354d9f772fba7eb17bd6d18d28480291dd93f85a18ca0a67ac2",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9f9e076b7e9752971f39eead6eda69df1c5e890c82ba2ca95f56974af7adfe79",
+    "zh:b1d6af047f96de7f97d38b685654f1aed4356d5060b0e696d87d0270f5d49f75",
+    "zh:bfb0654b6f34398aeffdf907b744af06733d168db610a2c5747263380f817ac7",
+    "zh:e25203ee8cedccf60bf450950d533d3c172509bda8af97dbc3bc817d2a503c57",
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,173 @@
+############################################################################
+## terraformブロック
+############################################################################
+terraform {
+  # Terraformのバージョン指定
+  # TODO:その日が来たらアプデする
+  required_version = "~> 1.7.0"
+
+  # Terraformのaws用ライブラリのバージョン指定
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"
+    }
+  }
+  
+  # 簡単な検証なので、ローカルにtfstateを保持する
+  backend "local" {
+    path = "local.tfstate"
+  }
+}
+
+############################################################################
+## providerブロック
+############################################################################
+provider "aws" {
+  # リージョンを指定
+  region = "ap-northeast-1"
+}
+
+############################################################################
+## localsブロック
+############################################################################
+locals{
+  project = "stepfunction-manage-cache"
+  
+  lambda_base_path = "../lambda"
+  lambda_redis = "operate-redis"
+  lambda_redis_path = "${local.lambda_base_path}/${local.lambda_redis}"
+
+  lambda_postgresql = "operate-posgresql"
+  lambda_postgresql_path = "${local.lambda_base_path}/${local.lambda_postgresql}"
+}
+
+############################################################################
+## VPC
+## 特に理由もないので、公式modulesを使用する
+## natも不要なので、intra subnetのみ作成する
+############################################################################
+module "intra_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.9.0" # TODO: providerと一緒に更新する
+
+  name = "${local.project}-intra-vpc"
+  cidr = "10.0.1.0/24"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
+  intra_subnets = ["10.0.1.0/25", "10.0.1.128/25"]
+  enable_nat_gateway = false
+}
+
+############################################################################
+## ElastieCache SSO Redis
+## valkeyはドキュメント少なそうだったので、とりあえずRedisを選択
+## クラスタなどは不要なので最小構成で作成する
+############################################################################
+# resource "aws_elasticache_subnet_group" "redis" {
+#   name       = "redis-cache-intra-subnet"
+#   subnet_ids = module.intra_vpc.intra_subnets
+# }
+
+# resource "aws_elasticache_cluster" "redis" {
+#   cluster_id           = "${local.project}-cluster-example"
+#   engine               = "redis"
+#   node_type            = "cache.t2.medium" # aws_elasticache_cluster.redis: Creation complete after 5m17s
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis7"
+#   engine_version       = "7.1"
+#   port                 = 6379
+#   subnet_group_name    = aws_elasticache_subnet_group.redis.name
+# }
+
+############################################################################
+## Lambda 2つ作る
+## 共通で参照するリソース群を定義
+############################################################################
+# lambda用AWSロール
+data "aws_iam_policy" "vpc_access_execution" {
+    arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+data "aws_iam_policy_document" "assume_lambda" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_vpc" {
+  name = "${local.project}-lambda-vpc-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc" {
+  role       = aws_iam_role.lambda_vpc.name
+  policy_arn = data.aws_iam_policy.vpc_access_execution.arn
+}
+
+# lambda用セキュリティグループ
+# VPC内のみ通信できればよい
+resource "aws_security_group" "lambda_vpc" {
+  name   = "${local.project}-lambda-vpc-sg"
+  vpc_id = module.intra_vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "lambda_vpc" {
+  security_group_id = aws_security_group.lambda_vpc.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [module.intra_vpc.vpc_cidr_block]
+}
+
+############################################################################
+## Lambda in VPC その１
+## redisを操作するlambda関数
+############################################################################
+# ログ残す
+resource "aws_cloudwatch_log_group" "redis" {
+  name              = "/aws/lambda/${local.lambda_redis}"
+  retention_in_days = 14
+}
+
+# zipを作成
+data "archive_file" "redis" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = local.lambda_redis_path
+  output_path      = "${local.lambda_base_path}/${local.lambda_redis}.zip"
+}
+
+resource "aws_lambda_function" "redis" {
+  function_name = local.lambda_redis
+  role          = aws_iam_role.lambda_vpc.arn
+
+  runtime  = "nodejs18.x"
+  filename = data.archive_file.redis.output_path
+  handler  = "index.handler"
+
+  source_code_hash = filebase64sha256(data.archive_file.redis.output_path)
+  
+  logging_config {
+    log_format = "Text"
+    log_group  = aws_cloudwatch_log_group.redis.name
+  }
+
+  vpc_config {
+    subnet_ids         = module.intra_vpc.intra_subnets
+    security_group_ids = [aws_security_group.lambda_vpc.id]
+  }
+
+  # zip内の変更は無視する
+  lifecycle {
+    ignore_changes = [filename, source_code_hash]
+  }
+}
