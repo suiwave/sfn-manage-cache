147df848991767fe94b24ed521a6fe86c0a809f2
suiwave <184898096+suiwave@users.noreply.github.com>
2024-10-16 18:18:24 +0900

    postgresql用lambda作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ locals{
   lambda_redis = "operate-redis"
   lambda_redis_path = "${local.lambda_base_path}/${local.lambda_redis}"
 
-  lambda_postgresql = "operate-posgresql"
+  lambda_postgresql = "operate-postgresql"
   lambda_postgresql_path = "${local.lambda_base_path}/${local.lambda_postgresql}"
 }
 
@@ -151,7 +151,60 @@ resource "aws_lambda_function" "redis" {
 
   environment {
     variables = {
-      REDIS_ENDPOINT = aws_elasticache_cluster.redis.cache_nodes[0].address
+      REDIS_ENDPOINT = "aws_elasticache_cluster.redis.cache_nodes[0].address"
+    }
+  }
+
+  # zip内の変更は無視する
+  lifecycle {
+    ignore_changes = [filename, source_code_hash]
+  }
+}
+
+############################################################################
+## Lambda in VPC その２
+## postgresqlを操作するlambda関数
+############################################################################
+# ログ残す
+resource "aws_cloudwatch_log_group" "postgresql" {
+  name              = "/aws/lambda/${local.lambda_postgresql}"
+  retention_in_days = 14
+}
+
+# zipを作成
+data "archive_file" "postgresql" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = local.lambda_postgresql_path
+  output_path      = "${local.lambda_base_path}/${local.lambda_postgresql}.zip"
+}
+
+resource "aws_lambda_function" "postgresql" {
+  function_name = local.lambda_postgresql
+  role          = aws_iam_role.lambda_vpc.arn
+
+  runtime  = "nodejs18.x"
+  filename = data.archive_file.postgresql.output_path
+  handler  = "index.handler"
+
+  source_code_hash = filebase64sha256(data.archive_file.postgresql.output_path)
+  
+  logging_config {
+    log_format = "Text"
+    log_group  = aws_cloudwatch_log_group.postgresql.name
+  }
+
+  vpc_config {
+    subnet_ids         = module.intra_vpc.intra_subnets
+    security_group_ids = [aws_security_group.lambda_vpc.id]
+  }
+
+  environment {
+    variables = {
+      DB_USER = "DB_USER"
+      RDS_ENDPOINT = "RDS_ENDPOINT"
+      DB_DATABASE = "DB_DATABASE"
+      DB_PASSWORD = "DB_PASSWORD"
     }
   }
 
@@ -166,37 +219,37 @@ resource "aws_lambda_function" "redis" {
 ## valkeyはドキュメント少なそうだったので、とりあえずRedisを選択
 ## クラスタなどは不要なので最小構成で作成する
 ############################################################################
-resource "aws_elasticache_subnet_group" "redis" {
-  name       = "redis-cache-intra-subnet"
-  subnet_ids = module.intra_vpc.intra_subnets
-}
+# resource "aws_elasticache_subnet_group" "redis" {
+#   name       = "redis-cache-intra-subnet"
+#   subnet_ids = module.intra_vpc.intra_subnets
+# }
 
-resource "aws_security_group" "redis" {
-  name   = "${local.project}-redis-sg"
-  vpc_id = module.intra_vpc.vpc_id
+# resource "aws_security_group" "redis" {
+#   name   = "${local.project}-redis-sg"
+#   vpc_id = module.intra_vpc.vpc_id
 
-  tags = {
-    name = "${local.project}-redis-sg"
-  }
-}
+#   tags = {
+#     name = "${local.project}-redis-sg"
+#   }
+# }
 
-resource "aws_security_group_rule" "redis" {
-  security_group_id = aws_security_group.redis.id
-  type              = "ingress"
-  from_port         = 6379
-  to_port           = 6379
-  protocol          = "TCP"
-  source_security_group_id = aws_security_group.lambda_vpc.id
-}
+# resource "aws_security_group_rule" "redis" {
+#   security_group_id = aws_security_group.redis.id
+#   type              = "ingress"
+#   from_port         = 6379
+#   to_port           = 6379
+#   protocol          = "TCP"
+#   source_security_group_id = aws_security_group.lambda_vpc.id
+# }
 
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${local.project}-cluster-example"
-  engine               = "redis"
-  node_type            = "cache.t2.medium" # aws_elasticache_cluster.redis: Creation complete after 5m17s
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
-  engine_version       = "7.1"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.redis.name
-  security_group_ids   = [aws_security_group.redis.id]
-}
+# resource "aws_elasticache_cluster" "redis" {
+#   cluster_id           = "${local.project}-cluster-example"
+#   engine               = "redis"
+#   node_type            = "cache.t2.medium" # aws_elasticache_cluster.redis: Creation complete after 5m17s
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis7"
+#   engine_version       = "7.1"
+#   port                 = 6379
+#   subnet_group_name    = aws_elasticache_subnet_group.redis.name
+#   security_group_ids   = [aws_security_group.redis.id]
+# }
